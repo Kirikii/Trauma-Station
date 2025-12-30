madeline-celeste/Maidstation

Fix float parse and infinity parsing in MegafaunaNumberSelector (and viviro fix) (#5528)
* culture

* power of mandemic mouse

* viviro fix

Content.Goobstation.Shared/Disease/Systems/SharedDiseaseSystem.Spread.cs
@@ -61,7 +61,7 @@ public partial class SharedDiseaseSystem
         if (power < 0 || chance < 0)
             return false;
 
-        if (_random.Prob(power * chance))
+        if (_random.Prob(Math.Min(power * chance, 1f)))
         {
             var infectDisease = disease;
             EntityUid? newDisease = null;

Kirikii/Trauma-Station

Content.Goobstation.Shared/Disease/Systems/SharedDiseaseSystem.Spread.cs
@@ -73,12 +73,7 @@ public partial class SharedDiseaseSystem
 
         var seed = SharedRandomExtensions.HashCodeCombine((int) _timing.CurTick.Value, GetNetEntity(target).Id);
         var rand = new System.Random(seed);
-        if (!rand.Prob(power * chance))
-            return false;
-
-        var infectDisease = disease;
-        EntityUid? newDisease = null;
-        if (clone)
+        if (rand.Prob(Math.Min(power * chance, 1f)))
         {
             newDisease = TryClone((disease, diseaseComp));
             if (newDisease == null)